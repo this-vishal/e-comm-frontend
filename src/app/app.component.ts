2ff0345743da3456a023d9ea3da4df9950a4aea1
vishal <vishalvino99@gmail.com>
2023-12-21 17:50:56 +0530

    navbar and curousel is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { CarouselComponent } from './components/carousel/carousel.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend'
 }
