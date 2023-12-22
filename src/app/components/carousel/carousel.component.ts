2a66531913cf8a13b8b53cf20b63fb68e551f60c
vishal <vishalvino99@gmail.com>
2023-12-22 19:53:19 +0530

    add carousel functionality

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-carousel',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+  @ViewChild('nextButton') nextButton: ElementRef | undefined
+  @ViewChild('prevButton') prevButton!: ElementRef
 
+  userCarouselClicked: boolean = false
+
+  ngOnInit () {
+    // Set up an interval to call nextSlide every 10 seconds
+    console.log(this.userCarouselClicked)
+
+    setInterval(() => {
+      if (!this.userCarouselClicked) {
+        this.nextSlide()
+      }
+    }, 2000)
+  }
+
+  nextSlide () {
+    this.nextButton?.nativeElement.click()
+  }
+
+  prevClick () {
+    this.userCarouselClicked = true
+    setTimeout(() => {
+      this.userCarouselClicked = false
+    }, 10000)
+  }
 }
---
+++ src/app/shared/card-container/card-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-container.component.html',
+  styleUrl: './card-container.component.css'
+})
+export class CardContainerComponent {
+
+}
